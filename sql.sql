16277d6e8ba2df44c97c1dc841cdb78d1a8b2a17
José Victor <jose.victor98@hotmail.com>
2023-03-14 13:43:23 +0000

    ...

---
+++ sql.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS desafio.produto (
+    id_produto string,
+    ds_produto string, 
+    id_subcategoria string
+    )
+
+COMMENT 'Tabela de Produto'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '|'
+STORED AS TEXTFILE
+location '/datalake/raw/produto/'
+TBLPROPERTIES ("skip.header.line.count"="1");
