e1060d2e58f9665553a6491bf04fc9fc3e56610b
José Victor <jose.victor98@hotmail.com>
2023-03-14 14:50:21 +0000

    commit final atividade

--- sql.sql
+++ sql.sql
@@ -1,12 +1,15 @@
-CREATE TABLE IF NOT EXISTS desafio.produto (
-    id_produto string,
-    ds_produto string, 
-    id_subcategoria string
-    )
+CREATE TABLE IF NOT EXISTS desafio.pedidos (
+    id_pedido string,
+    dt_pedito string,
+    id_parceiro string,
+    id_cliente string,
+    id_filial string,
+    vr_total_pago string
 
-COMMENT 'Tabela de Produto'
+)
+COMMENT 'Tabela de Pedidos'
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '|'
 STORED AS TEXTFILE
-location '/datalake/raw/produtos/'
-TBLPROPERTIES ("skip.header.line.count"="1");
+location '/datalake/raw/pedido/'
+TBLPROPERTIES ("skip.header.line.count"="1")
