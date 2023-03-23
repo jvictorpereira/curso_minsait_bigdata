90f49d10e8d472c729e7e552c9fb1d15a9f4c43c
José Victor <jose.victor98@hotmail.com>
2023-03-23 04:36:12 +0000

    commit 23/04

--- sql.sql
+++ sql.sql
@@ -1,3 +1,6 @@
+#So para formatação
+
+
 CREATE TABLE IF NOT EXISTS desafio.pedidos (
     id_pedido string,
     dt_pedito string,
@@ -13,3 +16,28 @@ FIELDS TERMINATED BY '|'
 STORED AS TEXTFILE
 location '/datalake/raw/pedido/'
 TBLPROPERTIES ("skip.header.line.count"="1")
+
+
+
+select * from pedidos as p
+inner join item_pedido as ip on p.id_pedido = ip.id_pedido
+limit
+vav
+
+
+
+
+
+
+
+
+
+
+sql_filial = ''' select * from filia f
+         join cidade c on f.id_cidade = c.id_cidade
+         join estado e on f.id_estado = c.id_estado
+    '''
+
+df_filial = spark.sql(sql_filial)
+
+df_filial.show(10)
